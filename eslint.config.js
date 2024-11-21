99bdd0d46b2e314eace3c6e9a4bfb988cd54aef8
Tobi <t.stoeckel@campus.lmu.de>
2024-11-21 20:54:39 +0100

    fixed prettier

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@
 const eslint = require("@eslint/js");
 const tseslint = require("typescript-eslint");
 const angular = require("angular-eslint");
-const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
 
 module.exports = tseslint.config(
   {
@@ -41,5 +41,5 @@ module.exports = tseslint.config(
     ],
     rules: {},
   },
-  eslintPluginPrettierRecommended
+  eslintPluginPrettierRecommended,
 );
