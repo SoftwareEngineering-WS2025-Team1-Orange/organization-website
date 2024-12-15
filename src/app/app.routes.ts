c192373a79b37292e6c8f2b522b6e48a95689d3a
Tobi <t.stoeckel@campus.lmu.de>
2024-12-15 17:20:54 +0100

    added routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { OrganizationOverviewComponent } from './pages/organization-overview/organization-overview.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { LogInComponent } from './pages/log-in/log-in.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: LandingPageComponent },
+  { path: 'overview', component: OrganizationOverviewComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'login', component: LogInComponent },
+  { path: 'sign-up', component: SignUpComponent },
+];
---
+++ src/app/pages/organization-overview/organization-overview.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-organization-overview',
+  standalone: true,
+  imports: [],
+  templateUrl: './organization-overview.component.html',
+  styleUrl: './organization-overview.component.scss',
+})
+export class OrganizationOverviewComponent {}
