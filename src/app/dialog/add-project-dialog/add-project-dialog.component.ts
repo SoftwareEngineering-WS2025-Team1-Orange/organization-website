d196af33968e5cd650a1530f5740c67bfb8600de
Tobias <stoeckel@cip.ifi.lmu.de>
2025-01-09 12:39:45 +0100

    add project

---
+++ src/app/dialog/add-project-dialog/add-project-dialog.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from "@angular/material/button";
+import {
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle
+} from '@angular/material/dialog';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CreateProject, CreateProjectCategoryEnum } from '../../../api';
+import { MatSelectModule } from '@angular/material/select';
+
+@Component({
+  selector: 'app-add-project-dialog',
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    MatDialogActions,
+    MatDialogContent,
+    MatDialogTitle,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatDialogClose,
+    MatSelectModule,
+  ],
+  templateUrl: './add-project-dialog.component.html',
+  styleUrl: './add-project-dialog.component.scss',
+})
+export class AddProjectDialogComponent {
+  form = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    description: new FormControl('', [Validators.required]),
+    fundraising_goal: new FormControl(0, [Validators.required]),
+    target_date: new FormControl('', [Validators.required]),
+    category: new FormControl<CreateProjectCategoryEnum>('OTHER'),
+  });
+
+  constructor(private dialogRef: MatDialogRef<AddProjectDialogComponent>) {}
+
+  save() {
+    const data: CreateProject = {
+      name: this.form.controls.name.value ?? '',
+      description: this.form.controls.description.value ?? '',
+      fundraising_goal: this.form.controls.fundraising_goal.value ?? 0,
+      target_date: this.form.controls.target_date.value ?? '',
+      category: this.form.controls.category.value ?? 'OTHER',
+    };
+    this.dialogRef.close(data);
+  }
+
+  protected readonly CreateProjectCategoryEnum = CreateProjectCategoryEnum;
+  protected readonly Object = Object;
+}
--- src/app/pages/organization-overview/organization-overview.component.ts
+++ src/app/pages/organization-overview/organization-overview.component.ts
@@ -7,6 +7,7 @@ import { MatIconModule } from '@angular/material/icon';
 import {Router} from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { editNGOData, EditNgoDialogComponent } from '../../dialog/edit-ngo-dialog/edit-ngo-dialog.component';
+import { AddProjectDialogComponent } from '../../dialog/add-project-dialog/add-project-dialog.component';
 
 @Component({
   selector: 'app-organization-overview',
@@ -62,6 +63,18 @@ export class OrganizationOverviewComponent implements OnInit{
       this.ngoStorage.ngo = {...ngo, ...result};
       this.apiService.ngo.updateNGO(ngo.id, result).then();
     });
+  }
+
+  openAddProjectDialog() {
+    const ngo = this.ngoStorage.ngo;
+    if(!ngo){
+      return;
+    }
+
+    const ref= this.dialog.open(AddProjectDialogComponent, {});
+    ref.afterClosed().subscribe(result => {
+      console.log(result);
+    })
   }
 
 }
