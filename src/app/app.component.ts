425672f1e282cc61c8308b3d7489b88645cf1e88
Tobias <stoeckel@cip.ifi.lmu.de>
2025-01-09 10:35:50 +0100

    added token refresh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import {ApiService} from './services/api.service';
 import {NgoStorageService} from './services/ngo-storage.service';
 import {TOKEN_KEY} from './services/constants';
 import { ResponseToken} from '../api';
+import globalAxios from 'axios';
 
 @Component({
   selector: 'app-root',
@@ -22,6 +23,37 @@ export class AppComponent implements OnInit {
   {}
 
   ngOnInit(): void {
+    this.initRefreshToken();
+    this.loadUser().then();
+  }
+
+  initRefreshToken() {
+    globalAxios.interceptors.response.use(
+      response => response,
+      async error => {
+        const originalRequest = error.config;
+        console.log(error)
+        if (error.response.status !== 401
+          || originalRequest._retry){
+          return Promise.reject(error);
+        }
+        originalRequest._retry = true;
+        try {
+          const response = await this.apiService.auth.getToken({
+            grant_type: 'refreshToken',
+            client_id: 'string',
+            client_secret: 'string',
+          })
+          this.apiService.setToken(response.data);
+          return globalAxios(originalRequest);
+        } catch (refreshError) {
+          return Promise.reject(refreshError);
+        }
+      }
+    )
+  }
+
+  async loadUser() {
     const tokenString =  localStorage.getItem(TOKEN_KEY);
 
     if (!tokenString) {
@@ -29,10 +61,7 @@ export class AppComponent implements OnInit {
     }
 
     const token: ResponseToken = JSON.parse(tokenString);
-    this.loadUser(token).then();
-  }
 
-  async loadUser(token: ResponseToken) {
     this.apiService.setToken(token);
 
     const req = await this.apiService.ngo.ngoControllerGetMeV1();
