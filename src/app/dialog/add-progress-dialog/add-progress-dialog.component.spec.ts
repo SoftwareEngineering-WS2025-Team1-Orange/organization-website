e43035f89441d5199cef49103460dfe8dc74b013
Tobi <t.stoeckel@campus.lmu.de>
2025-01-15 11:07:59 +0100

    fixed tests

--- src/app/dialog/add-progress-dialog/add-progress-dialog.component.spec.ts
+++ src/app/dialog/add-progress-dialog/add-progress-dialog.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AddProgressDialogComponent } from './add-progress-dialog.component';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 describe('AddProgressDialogComponent', () => {
   let component: AddProgressDialogComponent;
@@ -9,6 +10,7 @@ describe('AddProgressDialogComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AddProgressDialogComponent],
+      providers: [{ provide: MAT_DIALOG_DATA, useValue: {} }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AddProgressDialogComponent);
